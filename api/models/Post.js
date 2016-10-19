05be30bcfa7501711accbaadd0387f65a899e649
Rodrigo Nehring <rodrigonehring@hotmail.com>
2016-10-19 09:43:46 -0200

    post class model + tests

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,41 +1,60 @@
 const mongoose = require('mongoose');
+const loadClass = require('mongoose-class-wrapper');
 
-const Schema = new mongoose.Schema({
+const postSchema = new mongoose.Schema({
   slug: { type: String, unique: true },
   title: String,
   content: String,
   image: String,
 }, { timestamps: true });
 
-Schema.statics.newPost = function({ title, content, image, slug }) {
-	return this.findOne({ slug })
-		.then(post => {
-			if (post) throw('already_exist');
-			return new this({title, content, image, slug}).save(err => err);
-		});
+// Create new class with model methods 
+class PostModel {
+
+  static newPost({ title, content, image, slug }) {
+  	let errors = [];
+  	if (!title) errors.push('missing_title');
+  	else if (title.length < 4) errors.push('missing_title');
+    if (!content) errors.push('missing_content');
+    if (!image) errors.push('missing_image');
+    if (!slug) errors.push('missing_slug');
+
+  	if (errors.length) throw errors;
+  	
+    return this.findOne({ slug })
+    	.then(post => {
+    		if (post) throw('already_exist');
+    		return new this({title, content, image, slug}).save(err => err);
+    	});
+  }
+
+  static getPost({ slug }) {
+  	return this.findOne({ slug })
+  		.then(post => {
+  			if (!post) throw('post_not_found');
+  			return post;
+  		});
+  }
+
+  static editPost(slugId, { title, slug, image, content }) {
+		return this.findOne({ slug: slugId })
+			.then(post => {
+				if (!post) throw('post_not_found');
+        if (title) post.title = title;
+        if (slug) post.slug = slug;
+        if (image) post.image = image;
+        if (content) post.content = content;
+
+				return post.save();
+			});
+	}
+
+	static removePost({ slug }) {
+		return this.findOne({ slug })
+			.remove();
+	}
+ 
 }
-
-Schema.statics.getPost = function({ slug }) {
-	return this.findOne({ slug })
-		.then(post => {
-			if (!post) throw('post_not_found');
-			return post;
-		});
-}
-
-Schema.statics.editPost = function({ slug }) {
-	return this.findOne({ slug })
-		.then(post => {
-			if (!post) throw('post_not_found');
-			return post;
-		});
-}
-
-Schema.statics.removePost = function({ slug }) {
-	return this.findOne({ slug })
-		.remove();
-}
-
-const Post = mongoose.model('Post', Schema);
-
-module.exports = Post;
+ 
+postSchema.plugin(loadClass, PostModel);
+module.exports = mongoose.model('Post', postSchema);
--- api/test/Post.js
+++ api/test/Post.js
@@ -28,27 +28,51 @@ describe('Post Model', () => {
       });
   });
 
-  // it('should edit a post', (done) => {
-  //   Post.editPost({ slug: 'slug-olar' })
-  //     .then(post => {
-  //       expect(post.slug).to.equal('slug-olar');
-  //       done();
-  //     });
-  // });
+  it('should edit a post title', (done) => {
+    Post.editPost('slug-olar', {
+      title: 'ae',
+    })
+    .then(post => {
+      expect(post.title).to.equal('ae');
+      done();
+    });
+  });
+
+  it('should edit a post slug', (done) => {
+    Post.editPost('slug-olar', {
+      slug: 'nova-slug',
+    })
+    .then(post => {
+      expect(post.slug).to.equal('nova-slug');
+      done();
+    });
+  });
 
   it('should remove post', (done) => {
-    Post.removePost({ slug: 'slug-olar' })
+    Post.removePost({ slug: 'nova-slug' })
     .then(() => {
       done();
     })
   });
 
   it('should not find post by slug, because is deleted', (done) => {
-    Post.getPost({ slug: 'slug-olar' })
+    Post.getPost({ slug: 'nova-slug' })
       .catch(err => {
         expect(err).to.be.equal('post_not_found');
         done()
       });
   });
+
+
+
+    // it('should not create a new post', done => {
+    //   Post.newPost({title: 'olar'})
+    //   .catch(err => {
+    //     expect(err).to.be.equal('post_not_found');
+    //     done();
+    //   });
+    // });
+
+
 
 });
