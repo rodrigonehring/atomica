7220fe10b515e9640a5028ed2a9486801f0728db
Rodrigo Nehring <rodrigonehring@hotmail.com>
2016-10-20 09:14:33 -0200

    babel

--- api/config/db.js
+++ api/config/db.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 dotenv.load({ path: '.env' });
+
 /**
  * Connect to MongoDB.
  */
-
 mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI);
 mongoose.connection.on('error', () => {
   console.error('MongoDB Connection Error. Please make sure that MongoDB is running. db.js');
   process.exit(1);
 });
 
-module.exports = mongoose;
+export default mongoose;
